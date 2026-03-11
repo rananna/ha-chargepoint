28693d65054f52746d5a0180ea2e1c9f7f354d3d
rananna <rnorthca@gmail.com>
2026-03-11 08:32:20 -0400

    Refactor ChargePoint button implementation

--- custom_components/chargepoint/button.py
+++ custom_components/chargepoint/button.py
@@ -1,6 +1,7 @@
+"""Button platform for ChargePoint."""
 import logging
 from datetime import datetime
-from typing import Any, List, Optional, Tuple, Type
+from typing import Any, Optional, Type
 
 from homeassistant.components.button import (
     ButtonDeviceClass,
@@ -12,88 +13,70 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from python_chargepoint.exceptions import ChargePointCommunicationException
 
-from . import ChargePointChargerEntity, ChargePointEntityRequiredKeysMixin
+from . import ChargePointChargerEntity
 from .const import (
     ACCT_HOME_CRGS,
     DATA_CLIENT,
     DATA_COORDINATOR,
     DOMAIN,
-    EXCEPTION_WARNING_MSG,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
-
-class ChargePointChargerButtonEntity(ButtonEntity, ChargePointChargerEntity):
+class ChargePointChargerButtonEntity(ChargePointChargerEntity, ButtonEntity):
     """Representation of a ChargePoint Charger Device Button."""
-
-    entity_description: ButtonEntityDescription
 
     def __init__(
         self,
-        hass: HomeAssistant,
         client: Any,
         coordinator: Any,
         description: ButtonEntityDescription,
         charger_id: str,
     ) -> None:
-        """Initialize account sensor."""
+        """Initialize the button."""
         super().__init__(client, coordinator, charger_id)
-        self.hass: HomeAssistant = hass
-        self.entity_description: ButtonEntityDescription = description
-        self.last_toggled_on: Optional[datetime] = None
-
-        self._attr_name = f"{self.short_charger_model} {description.name_suffix}"
+        self.entity_description = description
+        self._attr_name = f"{self.short_model} {description.name_suffix}"
         self._attr_unique_id = f"{charger_id}_{description.key}"
-
-    async def _async_press(self) -> None:
-        pass
 
     async def async_press(self) -> None:
         """Press the button."""
-        await self.on_press()
-        self.last_toggled_on = datetime.now()
+        # This is the standard HA method for buttons
+        await self._handle_press()
         await self.coordinator.async_request_refresh()
 
+    async def _handle_press(self) -> None:
+        """Subclasses must implement this."""
+        raise NotImplementedError()
 
-class ChargePointChargerRestartChargerButton(ChargePointChargerButtonEntity):
-    """Specific entity for restarting the charger"""
-
-    async def _async_press(self) -> None:
+class ChargePointRestartButton(ChargePointChargerButtonEntity):
+    """Button to restart the home charger."""
+    async def _handle_press(self) -> None:
         try:
-            self.session = await self.hass.async_add_executor_job(
+            await self.hass.async_add_executor_job(
                 self.client.restart_home_charger, self.charger_id
             )
-        except ChargePointCommunicationException:
-            _LOGGER.exception(EXCEPTION_WARNING_MSG)
+        except Exception as err:
+            _LOGGER.error("Failed to restart charger %s: %s", self.charger_id, err)
 
+class ChargePointStartButton(ChargePointChargerButtonEntity):
+    """Button to start a charging session."""
+    async def _handle_press(self) -> None:
+        try:
+            await self.hass.async_add_executor_job(
+                self.client.start_charging_session, self.charger_id
+            )
+        except Exception as err:
+            _LOGGER.error("Failed to start charge for %s: %s", self.charger_id, err)
 
-class ChargePointChargerButtonEntityDescription(
-    ButtonEntityDescription, ChargePointEntityRequiredKeysMixin
-):
-    """Button entity description with required fields"""
-
-    def __init__(self, **kwargs) -> None:
-        self.name_suffix = kwargs.pop("name_suffix")
-        super().__init__(**kwargs)
-
-
-CHARGER_BUTTONS: List[
-    Tuple[
-        Type[ChargePointChargerButtonEntity], ChargePointChargerButtonEntityDescription
-    ]
-] = [
-    (
-        ChargePointChargerRestartChargerButton,
-        ChargePointChargerButtonEntityDescription(
-            key="restart_charger",
-            name_suffix="Restart Charger",
-            device_class=ButtonDeviceClass.RESTART,
-            icon="mdi:restart",
-        ),
-    ),
-]
-
+class ChargePointStopButton(ChargePointChargerButtonEntity):
+    """Button to stop a charging session."""
+    async def _handle_press(self) -> None:
+        if self.session:
+            try:
+                await self.hass.async_add_executor_job(self.session.stop)
+            except Exception as err:
+                _LOGGER.error("Failed to stop charge for %s: %s", self.charger_id, err)
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -101,16 +84,42 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the buttons."""
-
     client = hass.data[DOMAIN][config_entry.entry_id][DATA_CLIENT]
     coordinator = hass.data[DOMAIN][config_entry.entry_id][DATA_COORDINATOR]
 
-    entities: list[ButtonEntity] = []
+    entities = []
 
     for charger_id in coordinator.data[ACCT_HOME_CRGS].keys():
-        for button_class, description in CHARGER_BUTTONS:
-            entities.append(
-                button_class(hass, client, coordinator, description, charger_id)
-            )
+        # Restart Button
+        entities.append(ChargePointRestartButton(
+            client, coordinator, 
+            ButtonEntityDescription(
+                key="restart_charger",
+                name_suffix="Restart Charger",
+                device_class=ButtonDeviceClass.RESTART,
+                icon="mdi:restart",
+            ), 
+            charger_id
+        ))
+        # Start Button
+        entities.append(ChargePointStartButton(
+            client, coordinator,
+            ButtonEntityDescription(
+                key="start_charge",
+                name_suffix="Start Charge",
+                icon="mdi:play-circle",
+            ),
+            charger_id
+        ))
+        # Stop Button
+        entities.append(ChargePointStopButton(
+            client, coordinator,
+            ButtonEntityDescription(
+                key="stop_charge",
+                name_suffix="Stop Charge",
+                icon="mdi:stop-circle",
+            ),
+            charger_id
+        ))
 
     async_add_entities(entities)
