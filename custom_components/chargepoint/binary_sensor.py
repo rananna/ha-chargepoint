304c955c9cadcd2c2d20016a21e2c10e5c5b702f
rananna <rnorthca@gmail.com>
2026-03-11 07:48:12 -0400

    Update binary_sensor.py

--- custom_components/chargepoint/binary_sensor.py
+++ custom_components/chargepoint/binary_sensor.py
@@ -1,19 +1,49 @@
-from homeassistant.components.binary_sensor import BinarySensorDeviceClass, BinarySensorEntity
-from . import ChargePointChargerEntity
-from .const import DATA_CLIENT, DATA_COORDINATOR, DOMAIN
+"""Binary sensor platform for ChargePoint."""
+import logging
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-async def async_setup_entry(hass, entry, async_add_entities):
+from .const import DATA_CLIENT, DATA_COORDINATOR, DOMAIN
+from . import ChargePointChargerEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the ChargePoint binary sensors."""
     data = hass.data[DOMAIN][entry.entry_id]
-    entities = [ChargePointPluggedInBinary(data["client"], data["coordinator"], cid) 
-                for cid in data["coordinator"].data["home_chargers"].keys()]
+    client = data[DATA_CLIENT]
+    coordinator = data[DATA_COORDINATOR]
+
+    entities = []
+    # Ensure we use the correct key from your updated const.py
+    from .const import ACCT_HOME_CRGS
+    
+    for charger_id in coordinator.data[ACCT_HOME_CRGS].keys():
+        entities.append(ChargePointPluggedInBinary(client, coordinator, charger_id))
+
     async_add_entities(entities)
 
 class ChargePointPluggedInBinary(ChargePointChargerEntity, BinarySensorEntity):
+    """Binary sensor to show if the EV is plugged in."""
+
     _attr_device_class = BinarySensorDeviceClass.PLUG
+    
     def __init__(self, client, coordinator, charger_id):
         super().__init__(client, coordinator, charger_id)
-        self._attr_name = f"EV Connected ({self.short_model})"
+        # short_model is inherited from ChargePointChargerEntity in __init__.py
+        self._attr_name = f"{self.short_model} Plugged In"
         self._attr_unique_id = f"{charger_id}_plugged_in_binary"
+
     @property
     def is_on(self) -> bool:
+        """Return True if the vehicle is plugged in."""
         return self.charger_status.plugged_in
