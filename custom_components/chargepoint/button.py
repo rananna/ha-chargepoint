dfe611f9d8d0064df0555ad8de40b43e50b53bf5
rananna <rnorthca@gmail.com>
2026-03-12 05:14:58 -0400

    Refactor ChargePoint button functionality and entities

--- custom_components/chargepoint/button.py
+++ custom_components/chargepoint/button.py
@@ -1,34 +1,27 @@
 import logging
+from dataclasses import dataclass
+from typing import Callable
 from homeassistant.components.button import ButtonDeviceClass, ButtonEntity, ButtonEntityDescription
-from . import ChargePointChargerEntity
+from homeassistant.helpers.entity import EntityCategory
+from . import ChargePointChargerEntity, ChargePointEntityRequiredKeysMixin
 from .const import ACCT_HOME_CRGS, DATA_CLIENT, DATA_COORDINATOR, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-class ChargePointBaseButton(ChargePointChargerEntity, ButtonEntity):
+@dataclass(kw_only=True)
+class ChargePointButtonEntityDescription(ButtonEntityDescription, ChargePointEntityRequiredKeysMixin):
+    press_action: Callable[[any], any]
+
+class ChargePointButton(ChargePointChargerEntity, ButtonEntity):
+    entity_description: ChargePointButtonEntityDescription
     def __init__(self, client, coordinator, description, charger_id):
         super().__init__(client, coordinator, charger_id)
         self.entity_description = description
         self._attr_name, self._attr_unique_id = f"{self.short_model} {description.name_suffix}", f"{charger_id}_{description.key}"
-    async def async_press(self):
-        await self._handle_press()
-        await self.coordinator.async_request_refresh()
-
-class RestartButton(ChargePointBaseButton):
-    async def _handle_press(self): await self.hass.async_add_executor_job(self.client.restart_home_charger, self.charger_id)
-
-class StartButton(ChargePointBaseButton):
-    async def _handle_press(self): await self.hass.async_add_executor_job(self.client.start_charging_session, self.charger_id)
-
-class StopButton(ChargePointBaseButton):
-    async def _handle_press(self):
-        if self.session: await self.hass.async_add_executor_job(self.session.stop)
+    async def async_press(self) -> None:
+        await self.entity_description.press_action(self)
 
 async def async_setup_entry(hass, entry, async_add_entities):
     client, coordinator = hass.data[DOMAIN][entry.entry_id][DATA_CLIENT], hass.data[DOMAIN][entry.entry_id][DATA_COORDINATOR]
-    entities = []
-    for cid in coordinator.data[ACCT_HOME_CRGS].keys():
-        entities.append(RestartButton(client, coordinator, ButtonEntityDescription(key="restart", name_suffix="Restart", device_class=ButtonDeviceClass.RESTART), cid))
-        entities.append(StartButton(client, coordinator, ButtonEntityDescription(key="start", name_suffix="Start", icon="mdi:play"), cid))
-        entities.append(StopButton(client, coordinator, ButtonEntityDescription(key="stop", name_suffix="Stop", icon="mdi:stop"), cid))
-    async_add_entities(entities) 
+    entities = [ChargePointButton(client, coordinator, ChargePointButtonEntityDescription(key="restart", name_suffix="Restart", device_class=ButtonDeviceClass.RESTART, entity_category=EntityCategory.DIAGNOSTIC, press_action=lambda e: e.hass.async_add_executor_job(e.client.reboot_home_charger, e.charger_id)), cid) for cid in coordinator.data[ACCT_HOME_CRGS].keys()]
+    async_add_entities(entities)
