98181e4526a870bb87ad6ddd80eef78a61db4598
rananna <rnorthca@gmail.com>
2026-03-12 04:59:28 -0400

    Bump version to 1.1.8 and add poll interval options
    Updated version to 1.1.8 and added POLL_INTERVAL_OPTIONS.

--- custom_components/chargepoint/const.py
+++ custom_components/chargepoint/const.py
@@ -3,7 +3,7 @@ from homeassistant.const import Platform
 
 NAME = "ChargePoint (Custom Stealth)"
 DOMAIN = "chargepoint"
-VERSION = "1.1.7"
+VERSION = "1.1.8"
 ISSUE_URL = "https://github.com/rananna/ha-chargepoint/issues"
 
 PLATFORMS: list[Platform] = [
@@ -17,6 +17,13 @@ PLATFORMS: list[Platform] = [
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 OPTION_POLL_INTERVAL = "poll_interval"
+
+# Fixes config_flow.py ImportError
+POLL_INTERVAL_OPTIONS = {
+    900: "15 minutes",
+    1800: "30 minutes",
+    3600: "1 hour",
+}
 POLL_INTERVAL_DEFAULT = 900
 
 TOKEN_FILE_NAME = "chargepoint_session.json"
